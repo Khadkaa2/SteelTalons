e9a1f32f31e7205c3440850fa0639cf9c3f2c555
ka12114242 <aayushkhadka274@gmail.com>
2025-10-06 10:23:27 -0500

    6

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/TeleOpControlled.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/TeleOpControlled.java
@@ -24,11 +24,17 @@ import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
+import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
+
 import org.firstinspires.ftc.teamcode.SharedData;
 import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
 import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+
+import com.pedropathing.ftc.InvertedCoordinates;
+import com.pedropathing.ftc.PoseConverter;
+import com.pedropathing.ftc.FTCCoordinates;
 
 
 import java.util.ArrayList;
