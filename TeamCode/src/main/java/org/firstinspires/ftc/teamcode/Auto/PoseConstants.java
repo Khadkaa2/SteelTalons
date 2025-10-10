bc1c64e94d6b7672729c8111dcd72f9a2aecaac2
leviemery91 <levi@emeryzone.com>
2025-10-10 14:07:28 -0500

    Attempted to match coordinate system to Pedro System

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/PoseConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/PoseConstants.java
@@ -5,15 +5,15 @@ import com.bylazar.configurables.annotations.Configurable;
 import com.pedropathing.geometry.Pose;
 @Configurable
 public class PoseConstants {
-    public static Pose START_POSE = new Pose(0,-48,Math.toRadians(180));
-    public static Pose LAUNCH_POSE = new Pose(6,-48,Math.toRadians(150));
-    public static Pose ALIGN1_POSE = new Pose(27,-31.5,Math.toRadians(90));
-    public static Pose PICKUP1_POSE = new Pose(27,-12, Math.toRadians(90));
-    public static Pose ALIGN2_POSE = new Pose(51,-31.5,Math.toRadians(90));
-    public static Pose PICKUP2_POSE = new Pose(51, -12, Math.toRadians(90));
-    public static Pose END_POSE = new Pose(12,-12,Math.toRadians(180));
+    public static Pose START_POSE = new Pose(48,0,Math.toRadians(270));
+    public static Pose LAUNCH_POSE = new Pose(48,6,Math.toRadians(240));
+    public static Pose ALIGN1_POSE = new Pose(31.5,27,Math.toRadians(180));
+    public static Pose PICKUP1_POSE = new Pose(12,27, Math.toRadians(180));
+    public static Pose ALIGN2_POSE = new Pose(31.5,51,Math.toRadians(180));
+    public static Pose PICKUP2_POSE = new Pose(12, 51, Math.toRadians(180));
+    public static Pose END_POSE = new Pose(12,12,Math.toRadians(240));
     public static Pose FIELD_OFFSET = new Pose(8.75,8.75);
     public static Pose CamOff = new Pose(-8,3.5);
-    public static Pose parkPose = new Pose(23.5, -29.625,0);
+    public static Pose parkPose = new Pose(29.625, 23.5,0);
 
 }
