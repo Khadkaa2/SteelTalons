59dc57bf980e6468475ec92afe93c9d2c2ace67c
ka12114242 <aayushkhadka274@gmail.com>
2025-11-03 11:23:27 -0600

    some of NewAuto.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/NewAuto.java
@@ -0,0 +1,134 @@
+package org.firstinspires.ftc.teamcode.Auto;
+
+import android.graphics.Color;
+
+import androidx.xr.runtime.math.Pose;
+
+import com.bylazar.telemetry.PanelsTelemetry;
+import com.pedropathing.follower.Follower;
+import com.pedropathing.paths.Path;
+import com.pedropathing.paths.PathChain;
+import com.pedropathing.util.Timer;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.teamcode.ColorSensed;
+import org.firstinspires.ftc.teamcode.SharedData;
+import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagGameDatabase;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+
+import java.util.List;
+
+@Autonomous
+public class NewAuto extends OpMode {
+
+    private Follower f;
+    public PanelsTelemetry panels = PanelsTelemetry.INSTANCE;
+    private int pathState;
+    private Timer pathTimer, actionTimer,opmodeTimer,colorTimer,launchTimer,intakeTimer,detectColorTimer
+    private PoseConstants poses = new PoseConstants();
+    Pose currentPose = null;
+    private Path start, end;
+    private PathChain one, two, three, four, five, six;
+    private static AprilTagProcessor aprilTag;
+    private VisionPortal visionPortal;
+    int index;
+
+    boolean launching = false , launch = true;
+
+    int timesLaunched = 0;
+    private ColorSensed previousColor = ColorSensed.NO_COLOR, currentColor = ColorSensed.NO_COLOR;
+
+    int slotGoal = 0;
+
+    @Override
+    public void init() {
+        SharedData.reset();
+        initAprilTag();
+        pathTimer = new Timer();
+        actionTimer = new Timer();
+        opmodeTimer = new Timer();
+        intakeTimer = new Timer();
+        detectColorTimer = new Timer();
+        colorTimer = new Timer();
+        launchTimer = new Timer();
+        opmodeTimer.resetTimer();
+
+        f = Constants.createFollower(hardwareMap);
+        f.setStartingPose(poses.START_POSE);
+        buildPaths();
+
+
+        index = 0;
+
+    }
+
+    @Override
+    public void init_loop() {
+    int ID = figureID();
+    if (ID == 21) index = 0;
+    else if (ID == 22) index = 1;
+    else if (ID == 23) index = 2;
+    SharedData.greenIndex = index;
+
+        telemetry.addData("Green Index", index );
+        telemetry.addData("Side", SharedData.red ? "Red" : "Blue");
+
+    }
+
+    @Override
+    public void start(){
+        sendPose();
+        opmodeTimer.resetTimer();
+        setPathState(0);
+    }
+    @Override
+    public void loop(){
+        f.update();
+        sendPose();
+        autoPathUpdates();
+
+        // panels.getTelemetry().addData("key", value);
+        // panels.getTelemetry().update();
+        if ((pathState == 3 || pathState == 4 || pathState == 6|| pathState ==7 ) && !launching){
+            Robot.
+        }
+
+
+    }
+
+
+
+
+
+
+
+    public void initAprilTag(){
+        aprilTag = new AprilTagProcessor.Builder()
+                .setDrawAxes(true)
+                .setOutputUnits(DistanceUnit.INCH, AngleUnit.DEGREES).setTagLibrary(AprilTagGameDatabase.getCurrentGameTagLibrary())
+                .build();
+        VisionPortal.Builder builder = new VisionPortal.Builder();
+        builder.setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"));
+        builder.addProcessor(aprilTag);
+        visionPortal = builder.build();
+    }
+    public static int figureID(){
+        List<AprilTagDetection> detections = aprilTag.getDetections();
+        for (AprilTagDetection detection : detections){
+            if(detection.id == 21||detection.id == 22||detection.id == 23)
+                return detection.id;
+        }
+        return -1;
+    }
+
+
+
+
+}
