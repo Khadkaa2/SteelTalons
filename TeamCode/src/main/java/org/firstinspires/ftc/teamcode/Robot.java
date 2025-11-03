b70f87834ec43804a86f9e49599db22a4db45eec
leviemery91 <levi@emeryzone.com>
2025-11-03 10:56:37 -0600

    robot class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -0,0 +1,116 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.bylazar.configurables.annotations.Configurable;
+import com.pedropathing.geometry.BezierPoint;
+import com.pedropathing.paths.PathConstraints;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import org.firstinspires.ftc.robotcore.external.JavaUtil;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
+import org.firstinspires.ftc.robotcore.external.navigation.Position;
+import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
+import org.firstinspires.ftc.teamcode.Auto.PoseConstants;
+import org.firstinspires.ftc.teamcode.ColorSensed;
+import org.firstinspires.ftc.teamcode.SharedData;
+import org.firstinspires.ftc.teamcode.pedroPathing.Tuning;
+import com.bylazar.telemetry.PanelsTelemetry;
+
+import com.bylazar.panels.Panels;
+import com.pedropathing.follower.Follower;
+import com.pedropathing.geometry.BezierLine;
+import com.pedropathing.geometry.Pose;
+import com.pedropathing.paths.PathConstraints;
+
+import com.pedropathing.paths.Path;
+import com.pedropathing.paths.PathChain;
+import com.pedropathing.util.Timer;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagGameDatabase;
+import org.firstinspires.ftc.vision.apriltag.AprilTagPoseFtc;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+public class Robot {
+    private static DcMotorEx fan = null;
+    private static DcMotorEx rightLaunch = null;
+    private static DcMotorEx leftLaunch = null;
+    private static CRServo intakeServo = null;
+    private static CRServo feeder = null;
+    private static ColorSensor entranceColor = null;
+    private static DistanceSensor distanceSensor = null;
+
+     public Robot(HardwareMap hardwareMap){
+        intakeServo = hardwareMap.get(CRServo.class, "intakeServo");
+        entranceColor = hardwareMap.get(ColorSensor.class, "intakeColorSensor");
+        fan = hardwareMap.get(DcMotorEx.class, "sortMotor");
+        feeder = hardwareMap.get(CRServo.class, "feederServo");
+        rightLaunch = hardwareMap.get(DcMotorEx.class, "rightLaunch");
+        leftLaunch = hardwareMap.get(DcMotorEx.class, "leftLaunch");
+        distanceSensor = hardwareMap.get(DistanceSensor.class, "distanceSensor");
+    }
+
+    public void setStoragePos(int slot, boolean intake) {
+        int ticks = 1426;
+        int absolutePos = fan.getCurrentPosition();
+        int relativePos = absolutePos % ticks;
+        int rotationOffset = absolutePos-relativePos;
+
+        int sign = (int)Math.signum(absolutePos == 0 ? 1 : absolutePos);
+
+        if (intake) {
+            if (slot == 0) {
+                if(relativePos <= ticks/2)
+                    fan.setTargetPosition(rotationOffset);
+                else
+                    fan.setTargetPosition(rotationOffset + ticks);
+            } else if (slot == 1) {
+                if(relativePos <= 5*ticks/6)
+                    fan.setTargetPosition(rotationOffset + ticks/3);
+                else
+                    fan.setTargetPosition(rotationOffset + 4*ticks/3);
+            } else if (slot == 2) {
+                if(relativePos >= ticks/6)
+                    fan.setTargetPosition(rotationOffset + 2*ticks/3);
+                else
+                    fan.setTargetPosition(rotationOffset - ticks/3);
+
+            }
+        }
+        else {
+            if (slot == 0) {
+                fan.setTargetPosition(rotationOffset + ticks/2);
+            } else if (slot == 1) {
+                if(relativePos >= ticks/3)
+                    fan.setTargetPosition(rotationOffset + 5*ticks/6);
+                else
+                    fan.setTargetPosition(rotationOffset - ticks/6);
+            } else if (slot == 2) {
+                if(relativePos >= 2*ticks/3)
+                    fan.setTargetPosition(rotationOffset + 7*ticks/6);
+                else
+                    fan.setTargetPosition(rotationOffset + ticks/6);
+            }
+        }
+    }
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/NewTeleOp.java
@@ -0,0 +1,65 @@
+package org.firstinspires.ftc.teamcode.TeleOp;
+
+import static com.qualcomm.robotcore.hardware.DcMotor.RunMode.STOP_AND_RESET_ENCODER;
+
+import android.graphics.Color;
+
+import com.bylazar.telemetry.PanelsTelemetry;
+import com.pedropathing.follower.Follower;
+import com.pedropathing.geometry.BezierLine;
+import com.pedropathing.geometry.PedroCoordinates;
+import com.pedropathing.ftc.InvertedFTCCoordinates;
+import com.pedropathing.ftc.PoseConverter;
+import com.pedropathing.ftc.FTCCoordinates;
+
+
+import com.pedropathing.geometry.Pose;
+import com.pedropathing.util.Timer;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.CRServo;
+
+import org.firstinspires.ftc.robotcore.external.JavaUtil;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
+import org.firstinspires.ftc.teamcode.Auto.PoseConstants;
+
+
+import com.pedropathing.paths.PathChain;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
+import org.firstinspires.ftc.teamcode.ColorSensed;
+import org.firstinspires.ftc.teamcode.Robot;
+import org.firstinspires.ftc.teamcode.SharedData;
+import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+@TeleOp
+public class NewTeleOp extends LinearOpMode{
+    Robot hornet;
+
+    public void runOpMode()
+    {
+        hornet = new Robot(hardwareMap);
+        waitForStart();
+        hornet.setStoragePos(1,true);
+    }
+}
