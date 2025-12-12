ce55d5212d68f944ca450d6123713cd3a9b5be3e
leviemery91 <levi@emeryzone.com>
2025-12-12 11:40:20 -0600

    poses

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/TwelveBallPoseConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/TwelveBallPoseConstants.java
@@ -23,10 +23,10 @@ public class TwelveBallPoseConstants {
     public Pose LAUNCH_POSE_CLOSE = !SharedData.red ? new Pose(56.75,80.75,Math.toRadians(312)) : new Pose(87.25,80.75,Math.toRadians(225));
 
     public Pose ALIGN1_POSE = !SharedData.red ? new Pose(45.75,59.25,Math.toRadians(180)) :  new Pose(102.25,59.25,Math.toRadians(0));
-    public Pose PICKUP1_POSE = !SharedData.red ? new Pose(16, 62.25, Math.toRadians(180)) :  new Pose(137, 62.25, Math.toRadians(0));
+    public Pose PICKUP1_POSE = !SharedData.red ? new Pose(16, 62.25, Math.toRadians(180)) :  new Pose(128, 62.25, Math.toRadians(0));
 
     public Pose ALIGN2_POSE = !SharedData.red ? new Pose(45.75,33.25,Math.toRadians(180)) :  new Pose(102,33.25,Math.toRadians(0));
-    public Pose PICKUP2_POSE = !SharedData.red ? new Pose(17.75,33.25, Math.toRadians(180)) : new Pose(126.25,33.25, Math.toRadians(0));
+    public Pose PICKUP2_POSE = !SharedData.red ? new Pose(17.75,33.25, Math.toRadians(180)) : new Pose(128,33.25, Math.toRadians(0));
 
     public Pose ALIGN3_POSE = !SharedData.red ? new Pose(45.75,81.25,Math.toRadians(180)) : new Pose(102,83,Math.toRadians(0));
     public Pose PICKUP3_POSE = !SharedData.red ? new Pose(17.75,81.25, Math.toRadians(180)) : new Pose(128,83, Math.toRadians(0));
